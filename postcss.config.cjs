57383a5ea763dc2471d7901710a7ff2796404b88
nicknick <nickback0@yandex.ru>
2026-04-18 18:01:38 +0300

    chore(deps): mantine ui deps

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {
+      variables: {
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "48em",
+      },
+    },
+  },
+};
