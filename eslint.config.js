4159b65ee6292e4b01338c926804cbefd35ed1f9
nicknick <nickback0@yandex.ru>
2026-04-18 20:39:16 +0300

    chore(deps): new deps, eslint config, ts config, base fsd structure

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import mantine from "eslint-config-mantine";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    ignores: ["**/*.{mjs,cjs,js,d.ts,d.mts}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      ...mantine,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    tsconfigRootDir: import.meta.dirname,
+  },
+]);
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
       variables: {
         "mantine-breakpoint-xs": "36em",
         "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "62em",
+        "mantine-breakpoint-lg": "75em",
+        "mantine-breakpoint-xl": "88em",
       },
     },
   },
