5434b99e2ce68a16f0617faaedf495f21f926e90
nicknick <nickback0@yandex.ru>
2026-04-19 23:26:23 +0300

    chore: removed mantine-ui deps, updated linters, postCSS

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import mantine from "eslint-config-mantine";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -16,7 +15,6 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      ...mantine,
     ],
     languageOptions: {
       ecmaVersion: 2020,
