353d0caefac365e19fc2b664166577f4c1fc11c9
nicknick <nickback0@yandex.ru>
2026-04-20 17:48:14 +0300

    chore: add timeout api interceptor, error interceptor

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,10 +2,17 @@ import axios from "axios";
 
 export const $api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 4000,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+/** force delay middleware: */
+$api.interceptors.request.use(async (config) => {
+  if (import.meta.env.DEV) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
+  return config;
 });
 
 $api.interceptors.response.use(
